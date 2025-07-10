e5983d08ed1c50c58104d799f41d3fba99751e72
Pedro I <pedroizumida@outlook.com>
2025-07-10 17:48:35 -0300

    redesign functions flow

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def log_time(on,message,time):
 
 
 def debug_code(debug,message,var=None):
-	if debug == 1: print(f"{message}: {var};")
+	if debug: print(f"{message}: {var};")
 
 
 def idx_name(var):
@@ -32,13 +32,16 @@ def check_element(list_i,list_j,list_unique):
 				list_unique.append(elem)
 
 
-def column_first_blank(dataframe,debug=False):
-	for i,item in enumerate(dataframe.columns):
-		if "Unnamed:" in str(item): idx = i
+def column_first_blank(header,collumn_find,debug=False):
+	idx = None
+	for i,item in enumerate(header):
+		if collumn_find in str(item): 
+			idx = i
+			break
 
-		if idx is None: idx = (len(dataframe.columns)+1)
+	if idx is None: idx = (len(header)+1)
 	
-	debug(debug, "First empty column number", idx)
+	debug_code(debug, "First empty column number", idx)
 	return idx
 
 
@@ -60,27 +63,28 @@ def is_num(value):
 
 	return x
 
-
-def drop_numbered_col(column_drop_num_name,column_drop,dataframe):
-	# Drop all numbered columns
-	if '1' in column_drop_num_name and (column_drop == ' ' or column_drop[0] == ''):
-		bool_array = [is_num(x) for x in dataframe.columns]
-		bool_array = np.array(bool_array)
-		dataframe = dataframe.loc[:, ~bool_array]
-	
+#drop_numbered_col
+def col_drop(column_drop,column_drop_number,dataframe):
 	# Drop just listed numnered columns
-	if not(column_drop == ' ' and column_drop[0] =='') and not '1' in column_drop_num_name:
+	if '1' in column_drop:
 		for column_name in column_drop:
 			if column_name in dataframe.columns: 
 				dataframe.drop(columns=column_name, inplace=True)
 
+	# Drop all numbered columns
+	#if (column_drop == ' ' or column_drop[0] == ''):
+	if '1' in column_drop_number:
+		bool_array = [is_num(x) for x in dataframe.columns]
+		bool_array = np.array(bool_array)
+		dataframe = dataframe.loc[:, ~bool_array]
+	
 
 # MARK: Transform Data
 def transform(
 	path_root,path_destination,name_sheet
-	,header_row, header_adjust_model
-	,column_skip=False,column_stop_first_blank=False,column_drop_num_name=False
-	,column_drop=False,column_not_null=False,column_add_file_control=False
+	,header_start_row, header_adjust_model
+	,column_skip=False,column_stop_first_blank=False,column_drop=False
+	,column_drop_number=False,column_not_null=False,column_add_file_control=False
 	,row_stop_first_blank=False,row_drop_duplicate=False,drop_thresh_blank=False
 	,post_merge=False,logtime=False,debug=False
 ):
@@ -91,7 +95,7 @@ def transform(
 			if os.path.isfile(filename):
 				debug_code(debug,"01->Filename",filename)
 				time_start = datetime.datetime.now()
-				excel_dict = pd.read_excel(io=filename,header=header_row,sheet_name=None,na_values=["","-"],dtype=str) #changed
+				excel_dict = pd.read_excel(io=filename,header=header_start_row,sheet_name=None,na_values=["","-"],dtype=str) #changed
 				log_time(logtime,"[Time] Read file",datetime.datetime.now()-time_start)
 				
 				for name, sheet in excel_dict.items():
@@ -100,12 +104,12 @@ def transform(
 							#print(name,list(sheet.columns))
 							debug_code(debug,"02->Sheet name", name)
 							
-							if column_skip != '': sheet = sheet.iloc[:,int(column_skip):]
+							if column_skip: sheet = sheet.iloc[:,column_skip:]
 							
 							if '1' in column_stop_first_blank:
 								time_start = datetime.datetime.now()
 								# debug_code(debug,"File columns", sheet.columns)
-								idx = column_first_blank(sheet.columns,"Unnamed:")
+								idx = column_first_blank(header=sheet.columns,collumn_find="Unnamed:",debug=debug)
 								debug_code(debug,"03->First blank column index", idx)
 								sheet = sheet.iloc[:, :idx]
 								log_time(logtime,"[Time] Get dataset until first blank column",datetime.datetime.now()-time_start)
@@ -113,7 +117,7 @@ def transform(
 							#debug_code(debug, "Datataframe informations", sheet.info())
 							debug_code(debug, "04->Datataframe shape", sheet.shape)
 							
-							drop_numbered_col(column_drop_num_name=column_drop_num_name,column_drop=column_drop, dataframe=sheet)
+							col_drop(column_drop=column_drop,column_drop_number=column_drop_number,dataframe=sheet)
 
 							#sheet.columns = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in sheet.columns]	
 							header_current = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in sheet.columns]	
@@ -138,20 +142,14 @@ def transform(
 								log_time(logtime,"07->[Time] Filter dataset",datetime.datetime.now()-time_start)
 							
 					
-							if drop_thresh_blank != '': 
+							if drop_thresh_blank != '':
+								drop_thresh_blank = int(drop_thresh_blank)
 								if drop_thresh_blank >= 1: drop_thresh_blank = drop_thresh_blank-1
 
 								debug_code(debug,"08->Remove row if blank column is",drop_thresh_blank)
 								time_start = datetime.datetime.now()
 								sheet.dropna(thresh=drop_thresh_blank-1, inplace=True)
 								log_time(logtime,"[Time] Remove empty row",datetime.datetime.now()-time_start)
-
-							# Deprecated
-							"""if not(len(column_drop) == 1 and column_drop[0] ==''):
-								for column_name in column_drop:
-									print(f"file: {filename}, column: {column_name}-=-=-=-=-=-=-=-=-=")
-									if column_name in sheet.columns: sheet.drop(columns=str(column_name), inplace=True)
-									print("done")"""
 
 							if '1' in row_drop_duplicate:
 								time_start = datetime.datetime.now()
@@ -170,6 +168,9 @@ def transform(
 
 							if '1' in post_merge: path_absolute_destination = os.path.join(path_destination,f"{str(count)}_{name}.feather")
 							else: path_absolute_destination = os.path.join(path_destination,f"{name}.feather")
+							
+							if not os.path.exists(path_destination): os.makedirs(path_destination)
+
 							debug_code(debug,"09->Save file in",path_absolute_destination)
 							sheet.to_feather(path_absolute_destination)
 
@@ -182,7 +183,8 @@ def transform(
 	
 
 # MARK: Format Header
-def header_format(path_temp,filename,idx_or_name=False,header_row=False,line_number=False,debug=False,logtime=False,add_file_columns=False,column_skip=False):
+#def header_format(path_temp,idx_or_name=False,header_start_row=False,row_skip=False,debug=False,logtime=False,add_file_columns=False,column_skip=False):
+def header_format(path_temp,column_add_file_control,debug=False,logtime=False):
 	try:
 		time_start =  datetime.datetime.now()
 		previous_header = []
@@ -190,16 +192,16 @@ def header_format(path_temp,filename,idx_or_name=False,header_row=False,line_num
 		unique_columns = []
 
 		for filename in sorted(os.listdir(path_temp)):
-			filename = os.path.join(path_temp,filename)
-			if os.path.isfile(filename):
-				if Path(filename).suffix == ".csv": df = pd.read_csv(filename, sep=";", nrows=line_number, dtype=str,encoding="utf-8-sig")
-				else: df = pd.read_excel(io=filename,header=header_row,sheet_name=idx_or_name,na_values=["","-"],dtype=str,nrows=line_number)
+			abs_path_file = os.path.join(path_temp,filename)
+			if os.path.isfile(abs_path_file):
+				#if Path(filename).suffix == ".csv": df = pd.read_csv(filename, sep=";", header=header_start_row, dtype=str,encoding="utf-8-sig")
+				#else: df = pd.read_excel(io=filename,skiprows=header_start_row,sheet_name=idx_or_name,na_values=["","-"],dtype=str,nrows=row_skip,engine="openpyxl")
 
-				if column_skip != '': df = df.iloc[column_skip:]
+				#if column_skip: df = df.iloc[column_skip:]
+				current_header = pd.read_feather(abs_path_file).columns.to_list()
+				#current_header = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in df.columns]
 
-				current_header = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in df.columns]
-
-				debug_code(debug,"01->filename", filename)
+				debug_code(debug,"01->abs_path_file", abs_path_file)
 
 				if len(unique_columns) > 0:
 					check_element(previous_header,current_header,unique_columns)
@@ -215,7 +217,7 @@ def header_format(path_temp,filename,idx_or_name=False,header_row=False,line_num
 
 		complete_header = [x for x in previous_header if x not in unique_columns] + unique_columns
 		
-		if '1' in add_file_columns:
+		if '1' in column_add_file_control:
 			complete_header.insert(0,complete_header.pop(complete_header.index('filename')))
 			complete_header.insert(1,complete_header.pop(complete_header.index('sheet')))
 			if "idx" in complete_header: complete_header.insert(0,complete_header.pop(complete_header.index('idx')))
@@ -239,7 +241,6 @@ def join(path_temp,header_standardized,path_destination,logtime,debug=False):
 		if os.path.isfile(filename):
 			debug_code(debug,"06->filename",filename)
 			df = pd.read_feather(filename)
-			df.columns = header_standardized # Duplicated columns error happens here, be carefull with header_standardize parameter!!
 			df = df.reindex(df.columns.union(header_standardized, sort=False), axis=1, fill_value=None)
 			df = df.reindex(header_standardized,axis=1)
 			df = df.dropna(axis=0, how="all")
@@ -272,30 +273,30 @@ def main():
 	# To extract data
 	windows_username= os.getlogin()
 	path_root = config["PATH"]["fileIn"].replace("custom",windows_username)
+
+	post_merge = config["MODE"]["post_merge"]
+	root_path = os.getcwd()
 	path_destination = config["PATH"]["fileOut"].replace("custom",windows_username)
-	path_temp = os.path.join(os.getcwd(),"temp")
+	
+	path_temp = os.path.join(root_path,"temp")
 	if not os.path.exists(path_temp): os.makedirs(path_temp)
 
-	# Transform and load data (T.L)
-	post_merge = config["MODE"]["post_merge"]
-	idx_or_name = idx_name(config["MODE"]["sheet_identification"])
-
-	filename = config["FILE"]["name"] + ".csv"
+	filename = config["FILE"]["name"] + ".feather"
 	name_sheet = config["FILE"]["name_sheet"].lower().split(",")
 	
-	header_row = config["HEADER"]["row"]
+	header_start_row = int(config["HEADER"]["start_row"])
 	header_adjust_model = config["HEADER"]["adjust_model"]
+	
 	if header_adjust_model != [""]:
 		v = [x.lower().split(',') for x in header_adjust_model.split(';')]
 		k = [x.pop(0) for x in v]
 		header_adjust_model = [k,v]
-	else: header_adjust_model = None
 
-	column_skip = config["COLUMN"]["skip"]
+	column_skip = int(config["COLUMN"]["skip"])
 	if column_skip > 2: column_skip = column_skip-1
 	column_not_null = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in config["COLUMN"]["not_null"].split(",")]
 	column_drop = [(''.join(letter for letter in unidecode(str(elem)) if letter.isalnum())).lower() for elem in config["COLUMN"]["drop"].split(",")]
-	column_drop_num_name = config["COLUMN"]["drop_num_name"]
+	column_drop_number = config["COLUMN"]["drop_number"]
 	column_stop_first_blank = config["COLUMN"]["stop_first_blank"]
 	column_add_file_control = config["COLUMN"]["add_file_control"]
 
@@ -303,40 +304,39 @@ def main():
 	row_drop_duplicate = config["ROW"]["drop_duplicate"]
 	drop_thresh_blank = config["ROW"]["drop_thresh_blank"]
 
-	# Debug/monitoring
 	logtime = config["DEBUG"]["logtime"]
 	debug= config["DEBUG"]["on"]
 	time_start =  datetime.datetime.now()
 
+	if '1' in post_merge: aux = path_temp
+	else: aux = path_destination
+
 	transform(
-		path_root,path_destination,name_sheet
-		,header_row, header_adjust_model
-		,column_skip=column_skip,column_stop_first_blank=column_stop_first_blank,column_drop_num_name=column_drop_num_name
-		,column_drop=column_drop,column_not_null=column_not_null,column_add_file_control=column_add_file_control
+		path_root=path_root,path_destination=aux,name_sheet=name_sheet
+		,header_start_row=header_start_row, header_adjust_model=header_adjust_model
+		,column_skip=column_skip,column_stop_first_blank=column_stop_first_blank,column_drop=column_drop
+		,column_drop_number=column_drop_number,column_not_null=column_not_null,column_add_file_control=column_add_file_control
 		,row_stop_first_blank=row_stop_first_blank,row_drop_duplicate=row_drop_duplicate,drop_thresh_blank=drop_thresh_blank
 		,post_merge=post_merge,logtime=logtime,debug=debug
 	)
 
-	if 1==0:
-		if not header_adjust_model is None:
-			header_standardized = header_format(path_temp,filename,idx_or_name=False,header_row=False,line_number=False,debug=False,logtime=False,add_file_columns=False,column_skip=column_skip)
-			join(path_temp,header_standardized,path_destination,logtime,debug=False)
-			delete_tempfile(path_temp,filename,logtime,debug=False)
+	if '1' in post_merge:
+		#header_standardized = header_format(path_temp,filename,idx_or_name=False,header_start_row=header_start_row,row_skip=row_skip,debug=False,logtime=logtime,add_file_columns='0',column_skip=column_skip)
+		header_standardized = header_format(path_temp=path_temp,column_add_file_control=column_add_file_control,debug=debug,logtime=logtime)
+		join(path_temp,header_standardized,os.path.join(path_destination,filename),logtime,debug=False)
+		delete_tempfile(path_temp,filename,logtime,debug=False)
 			
-		if '1' in config["CONFIG"]["save"]:
-			gcf.generate(
-				path_in=os.getcwd()
-				,path_out=config["CONFIG"]["path_out"].replace("custom",windows_username)
-				,name="config"
-				,name_new=config["FILE"]["name"]
-				,summary=config["CONFIG"]["summary"]
-			)
-	
+	if '1' in config["CONFIG"]["save"]:
+		gcf.generate(
+			path_in=os.getcwd()
+			,path_out=config["CONFIG"]["path_out"].replace("custom",windows_username)
+			,name="config"
+			,name_new=config["FILE"]["name"]
+			,summary=config["CONFIG"]["summary"]
+		)
+
 	log_time(logtime,"[Time] Start Time",time_start)
 	log_time(logtime,"[Time] Total spended",datetime.datetime.now()-time_start)
 
 
-if __name__ == "__main__":
-	print("Start")
-	main()
-	print("Finished")
+if __name__ == "__main__": main()
